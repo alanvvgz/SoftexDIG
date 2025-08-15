61be2b93fa00e2f3aa564fd6aca241b416a41dce
Alan Vitor <130258672+alanvvgz@users.noreply.github.com>
2025-08-14 21:22:16 -0300

    Create Exemplo 3 InfoProduto.py

---
+++ Exemplo 3 InfoProduto.py
@@ -0,0 +1,9 @@
+# 3) Informações do Produto
+
+nome_produto = "Lápis"
+preco_produto = 2.50
+quantidade_estoque = 100
+
+print(f"\nProduto: {nome_produto}")
+print(f"Preço: R$ {preco_produto:.2f}")
+print(f"Quantidade em estoque: {quantidade_estoque} unidades")
