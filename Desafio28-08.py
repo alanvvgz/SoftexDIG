2aebe8ffd9a0077975de4d4e226148f86e0de6fd
Alan Vitor <130258672+alanvvgz@users.noreply.github.com>
2025-08-28 22:37:06 -0300

    Add files via upload

---
+++ Desafio28-08.py
@@ -0,0 +1,9 @@
+pao = 0.75
+broa = 1.50
+
+qtd_pao = int(input("Quantos pães? "))
+qtd_broa = int(input("Quantas broas? "))
+
+total = (qtd_pao * pao) + (qtd_broa * broa)
+
+print("Total a pagar: R$", total)
