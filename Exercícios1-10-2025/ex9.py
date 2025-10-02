50770801908517cb2dfcb6a6e9e0060645e81c65
Alan Vitor <130258672+alanvvgz@users.noreply.github.com>
2025-10-02 19:46:47 -0300

    Update ex9.py

--- Exercícios1-10-2025/ex9.py
+++ Exercícios1-10-2025/ex9.py
@@ -1,7 +1,10 @@
-num = [2, 5, 4, 1, 9]
-def analisar_lista(numeros):
-    soma = sum(numeros)
-    maior = max(numeros)
-    return (soma, maior)
-resultado = analisar_lista(num)
-print(resultado)
+def contar_vogais(texto):
+    vogais = "aeiouAEIOUáÁéÉíÍóÓúÚâÂêÊôÔãÃõÕ"
+    contador = 0
+    for letra in texto:
+        if letra in vogais:
+            contador += 1
+    return contador
+
+frase = "O incrível fenômeno da ciência revelou átomos, elétrons, íons e moléculas complexas."
+print(contar_vogais(frase))
